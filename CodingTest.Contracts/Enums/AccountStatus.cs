5df567d0ea16d52f6b2da7a1824a2b1c198efe3f
Kevin <spam@madpoet.com>
2021-02-10 11:26:52 -0800

    Initial Commit

---
+++ CodingTest.Contracts/Enums/AccountStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest.Common.Enums
+{
+    public enum AccountStatus
+    {
+        Active,
+        Inactive,
+        Overdue
+    }
+}
---
+++ CodingTest.Contracts/Models/Accounts/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CodingTest.Common.Enums;
+
+namespace CodingTest.Common.Models.Accounts
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public decimal AmountDue { get; set; }
+
+        public DateTimeOffset PaymentDueDate { get; set; }
+
+        public AccountStatus Status { get; set; }
+    }
+}
